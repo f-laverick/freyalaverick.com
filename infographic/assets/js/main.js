4d02856b5be82ce4cec9a8fc553ee338a04a46ec
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2021-05-06 00:28:51 +0100

    Loads of stuff

--- infographic/assets/js/main.js
+++ infographic/assets/js/main.js
@@ -1,20 +1,25 @@
 (function () {
   'use strict';
 
+  let scrollPosition = 0,
+    windowBottom = 0,
+    $animatedElements = null,
+    nextElement = null;
+
   const animationInit = function () {
-    const $animatedElements = document.querySelectorAll('.box,.line span');
+    $animatedElements = document.querySelectorAll('.box,.line span');
 
     // Check if there are any animated elements
     if ($animatedElements.length > 0) {
       Array.prototype.forEach.call($animatedElements, function ($el) {
         // Add an animationend event on each of them
         $el.addEventListener('animationend', function (e) {
-          // Look for the element that should be animated next
-          const $nextElement = document.querySelector(
-            '#' + e.target.dataset.next
-          );
           // If a next element is found, start the animation on that
-          if ($nextElement) {
+          if (e.target.dataset.next) {
+            // Look for the element that should be animated next
+            const $nextElement = document.querySelector(
+              '#' + e.target.dataset.next
+            );
             $nextElement.classList.add('run');
           }
         });
@@ -26,17 +31,17 @@
   };
 
   const scrollInit = function () {
-    var scroll_position = 0,
-      tick = false,
+    let tick = false,
       $body = document.getElementsByTagName('body').item(0);
     window.addEventListener('scroll', function (e) {
-      scroll_position = window.scrollY;
+      scrollPosition = window.scrollY;
+      windowBottom = scrollPosition + window.outerHeight;
       if (!tick) {
         window.requestAnimationFrame(function () {
-          if (scroll_position > 0 && !$body.classList.contains('scrolling')) {
+          if (scrollPosition > 0 && !$body.classList.contains('scrolling')) {
             $body.classList.add('scrolling');
           }
-          if (scroll_position == 0) {
+          if (scrollPosition == 0) {
             $body.classList.remove('scrolling');
           }
           tick = false;
@@ -48,7 +53,15 @@
 
   const init = function () {
     scrollInit();
-    animationInit();
+    if (document.querySelector('.start')) {
+      animationInit();
+    }
+    const $videoPlayer = document.querySelector('#videoPlayer');
+    if ($videoPlayer) {
+      $videoPlayer.addEventListener('ended', (e) => {
+        document.querySelector('#videoText').classList.add('active');
+      });
+    }
   };
 
   if (
