98565bc10f94bfce5c8043f1141654e036945d54
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2021-05-05 14:50:27 +0100

    Started animation library, working on page 1

--- infographic/assets/js/main.js
+++ infographic/assets/js/main.js
@@ -1,57 +1,28 @@
 (function () {
   'use strict';
 
-  const navbarInit = function () {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0
-    );
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      Array.prototype.forEach.call($navbarBurgers, function ($el) {
-        // Get the target from the "data-target" attribute
-        const target = $el.dataset.target,
-          $target = document.getElementById(target);
-
-        // Add a click event on each of them
-        $el.addEventListener('click', function (e) {
-          // Toggle the 'is-active' class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-        });
-      });
-    }
-  };
-
-  const resetDropdownToggles = function ($toggles, $clicked) {
-    // Collapse all other dropdowns
-    Array.prototype.forEach.call($toggles, function ($toggle) {
-      if (
-        !$clicked.isSameNode($toggle) &&
-        $toggle.classList.contains('is-active')
-      ) {
-        // Remove the class on both the dropdown toggle and the dropdown target
-        $toggle.classList.remove('is-active');
-        $toggle.querySelector('.navbar-dropdown').classList.remove('is-active');
-      }
-    });
-  };
-
   const animationInit = function () {
-    const $animatedElements = document.querySelectorAll('.box,.line');
+    const $animatedElements = document.querySelectorAll('.box,.line span');
 
     // Check if there are any animated elements
     if ($animatedElements.length > 0) {
       Array.prototype.forEach.call($animatedElements, function ($el) {
         // Add an animationend event on each of them
         $el.addEventListener('animationend', function (e) {
-          const nextElement = e.target.dataset.next;
-          this.querySelector(nextElement).classList.add('run');
+          // Look for the element that should be animated next
+          const $nextElement = document.querySelector(
+            '#' + e.target.dataset.next
+          );
+          // If a next element is found, start the animation on that
+          if ($nextElement) {
+            $nextElement.classList.add('run');
+          }
         });
       });
     }
+
+    // Start the animation off on the start element
+    document.querySelector('.start').classList.add('run');
   };
 
   const scrollInit = function () {
