9d4273e83bb980025d062519515d2787517da45c
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-05-25 09:30:59 +0100

    Add full url to projects links

--- store/projects.js
+++ store/projects.js
@@ -430,7 +430,7 @@ export const state = () => ({
       copy: `
 <h3>Digital Publishing Project</h3>
 <p>A digital publishing project exploring the flexibility of a web based version of a magazine design. Working digitally allowed for a more interactive experience for the user.</p>
-<p><a href="/projects/kernel" target="_blank">view project here</a> <small>(opens in new tab)</small></p>
+<p><a href="https://freyalaverick.com/projects/kernel" target="_blank">view project here</a> <small>(opens in new tab)</small></p>
 
       `,
       images: [
@@ -473,7 +473,7 @@ export const state = () => ({
       copy: `
 <h3>Infographic Project</h3>
 <p>An infographic project exploring the band 'Shakespears Sister', using html, css and beginning to explore animation.</p>
-<p><a href="/projects/infographic" target="_blank">view infographic here</a> <small>(opens in new tab)</small></p>
+<p><a href="https://freyalaverick.com/projects/infographic" target="_blank">view infographic here</a> <small>(opens in new tab)</small></p>
 
       `,
       images: [
